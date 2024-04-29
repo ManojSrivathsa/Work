6166ab5cfffc64b7a2e8f332ded49dae49c72c23
ManojSrivathsa <167759228+ManojSrivathsa@users.noreply.github.com>
2024-04-29 14:56:33 +0530

    Add files via upload

--- try1.java
+++ try1.java
@@ -4,12 +4,14 @@ package Testtry;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
+import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Wait;
 import org.openqa.selenium.support.ui.WebDriverWait;
 public class try1 {
     static WebDriver driver;
@@ -20,30 +22,32 @@ public class try1 {
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		
 		// Launch the URL
-		driver.get("file:///C:/Users/Administrator/Desktop/qwerty.html");
-		
-		List<WebElement> rdBtn_ans= driver.findElements(By.name("ans"));
+		driver.get("file:///C:/Users/Administrator/Desktop/alert.html");
 		
 		
+		//Click the link to activate the alert
+		driver.findElement(By.linkText("Show confirm box")).click();
+
 		
-		// Create a boolean variable which will hold the value (True/False)
-		boolean bValue = false;
 		
-		// This statement will return True, in case of first Radio button is selected
-		bValue = rdBtn_ans.get(0).isSelected();
-		System.out.println(bValue);
-		
-		// This will check that if the bValue is True means if the first radio button is selected
-		if(bValue == true){
-			// This will select Second radio button, if the first radio button is selected by default
-			rdBtn_ans.get(1).click();
-		}else{
-			// If the first radio button is not selected by default, the first will be selected
-			rdBtn_ans.get(0).click();
+		//Wait for the alert to be displayed
+		//if(Wait.until(ExpectedConditions.alertIsPresent())==null)
+		{
+			System.out.println("alert was not present");
 		}
+		//else(System.out.println("alert was present"));
+
+		//Store the alert in a variable
+		Alert alert = driver.switchTo().alert();
+
+		//Store the alert in a variable for reuse
+		String text = alert.getText();
+
+		//Press the Cancel button
+		alert.dismiss();		}
 		
 		
 	
 		}
 
-}
+
